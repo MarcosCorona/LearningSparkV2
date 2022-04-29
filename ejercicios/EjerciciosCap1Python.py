b6e10d2f545647813b1c22c13da77cf4ac02fee6
MarcosCorona <91139292+MarcosCorona@users.noreply.github.com>
2022-04-29 13:03:27 +0200

    ajustes de nombre

--- ejercicios/EjerciciosCap1Python.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-quijote = spark.read.text("/FileStore/shared_uploads/marcos.corona@bosonit.com/el_quijote.txt")
-
-# COMMAND ----------
-
-quijote.show()
-
-# COMMAND ----------
-
